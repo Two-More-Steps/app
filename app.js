0652b5913923e87d5113bb28bdada65c0b84356b
Two-More-Steps <ss@twomos.com>
2022-10-11 18:17:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+
+loader.load(
+  "path/to/model.glb",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
